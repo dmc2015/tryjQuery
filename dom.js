4df6344b5c3fc3659be3949f7be19ecfac4332a9
Don McLamb <mclamb.donald@gmail.com>
2015-09-21 00:08:31 -0400

    added notes

---
+++ dom.js
@@ -0,0 +1,25 @@
+$(document).ready(function()  {
+
+  console.log('doc is ready');
+
+
+  $('.book').on('click', function() {
+    console.log('showNumber called');
+    var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
+    $('.usa').append(message);
+
+  })
+
+
+});
+
+
+
+$(document).ready(function()  {
+
+  $('.book').on('click', function() {
+    'code'
+  });
+
+
+});
